f7bac48e4d71404c89a330dc8b28cae48b6ce3d7
yelmokht <younes.el.mokhtari@ulb.be>
2025-04-26 17:12:31 +0200

    First version of bootstrapping key size

--- benchmark.cpp
+++ benchmark.cpp
@@ -5,6 +5,22 @@
 
 #define NUMBER_OF_EXECUTIONS 1
 
+float ntru_key_switching_key_size() {
+    return (sizeof(int) * parNTRU.n * parNTRU.l_ksk) / (1024.0);
+}
+
+float lwe_key_switching_key_size() {
+    return (sizeof(int) * parLWE.n * parLWE.l_ksk) / (1024.0);
+}
+
+float ntru_bootstrapping_key_size() {
+    return (2 * parNTRU.n * (parNTRU.l_bsk[0] + parNTRU.l_bsk[1]) * parNTRU.N * log2(parNTRU.q_base)) / (8 * 1000.0 * 1000.0);
+}
+
+float lwe_bootstrapping_key_size() {
+    return (parLWE.n * (parLWE.l_bsk[0] + parLWE.l_bsk[1]) * parLWE.N * log2(parLWE.q_base)) / (8 * 1000.0 * 1000.0);
+}
+
 int ntru_fft_multiplication() {
     int fft_number = parNTRU.bsk_partition[0] * (parNTRU.l_bsk[0] + 1) + parNTRU.bsk_partition[1] * (parNTRU.l_bsk[1] + 1);
     std::cout << "Number of FFTs: " << fft_number << std::endl;
@@ -83,15 +99,15 @@ void benchmark() {
     std::cout << "Average bootstrapping running time over 1000 executions: " << 66 << " ms" << std::endl;
     std::cout << "-------------------------" << std::endl;
     std::cout << "MNTRU tests" << std::endl;
-    std::cout << "Key switching key size: " << (sizeof(int)*parNTRU.n*parNTRU.l_ksk)/1024.0 << " MB" << std::endl;
-    std::cout << "Bootstrapping key size: " <<  (sizeof(int)*parNTRU.n*(parNTRU.l_bsk[0] + parNTRU.l_bsk[1]))/1024.0 << " MB" << std::endl; // à corriger
+    std::cout << "Key switching key size: " << ntru_key_switching_key_size() << " MB" << std::endl;
+    std::cout << "Bootstrapping key size: " <<  ntru_bootstrapping_key_size() << " MB" << std::endl; // à corriger
     std::cout << "Mutliplication on R_Q: " << ntru_ring_multiplication() << std::endl;
     std::cout << "Number of FFTs: " << ntru_fft_multiplication() << std::endl;
     std::cout << "Average bootstrapping running time over 1000 executions: " << average_bootstrapping_running_time("NTRU") << " s" << std::endl;
     std::cout << "-------------------------" << std::endl;
     std::cout << "LWE tests" << std::endl;
-    std::cout << "Key switching key size: " << (sizeof(int)*parLWE.n*parLWE.l_ksk)/1024.0 << "MB" << std::endl;
-    std::cout << "Bootstrapping key size: " << (sizeof(int)*parLWE.n*(parLWE.l_bsk[0] + parLWE.l_bsk[1]))/1024.0 << "MB" << std::endl; // à corriger
+    std::cout << "Key switching key size: " << lwe_key_switching_key_size() << "MB" << std::endl;
+    std::cout << "Bootstrapping key size: " << lwe_bootstrapping_key_size() << "MB" << std::endl; // à corriger
     std::cout << "Mutliplication on R_Q: " << lwe_ring_multiplication() << std::endl;
     std::cout << "Number of FFTs: " << lwe_fft_multiplication() << std::endl;
     std::cout << "Average bootstrapping running time over 1000 executions: " << average_bootstrapping_running_time("LWE") << " s" << std::endl;
